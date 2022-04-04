a43325f5745be2c7ffa32e17f32f90f7e7937d66
Venkatesh Kulkarni <59998901+itzinstavenky@users.noreply.github.com>
2022-04-04 20:52:23 +0530

    Update mux_test.v

--- mux/mux_test.v
+++ mux/mux_test.v
@@ -1,1 +1,19 @@
+module test;
+reg in1, in2, select;
+wire out;
 
+mux2_1 mux(.in1(in1), .in2(in2), 
+            .select(select), .out(out));
+
+initial begin in1=1'b0;in2=1'b0;select=1'b0; 
+        #2    in1=1'b1;
+        #2    select=1'b1;
+        #2    in2=1'b1;
+        #2    $stop();
+        end
+
+initial begin $monitor("time=%0d, input1=%b, input2=%b, 
+                       select line=%b, output=%b", $time, 
+                        in1, in2, select, out);
+        end
+endmodule
